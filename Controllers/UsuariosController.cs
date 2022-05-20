7df91549a5f96aeb60aa59c8af42baed1fb8fc6e
Gabrielly-passos <passosgabrielly03@gmail.com>
2022-05-19 22:33:28 -0300

    [UPDATE]Aula Inicial Frontend

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -1,4 +1,12 @@
+using RpgMvc.Models;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
 
 namespace RpgMvc.Controllers
 {
@@ -9,10 +17,75 @@ namespace RpgMvc.Controllers
         
     [HttpGet]
 
-    public  ActionResult Index()
-    {
-        return View("CadastrarUsuario"); 
-    }
+        public  ActionResult Index()
+        {
+            return View("CadastrarUsuario"); 
+        }
+
+    [HttpPost]
+        public async Task<ActionResult> RegistrarAsync(UsuarioViewModel u)
+        {
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                string uriComplementar = "Registrar";
+
+                var content = new StringContent(JsonConvert.SerializeObject(u));
+                content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = await httpClient.PostAsync(uriBase + uriComplementar, content);
+
+                string Serialized = await response.Content.ReadAsStringAsync();
+
+                if(response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    TempData["Mensagem"] =
+                    string.Format("Usuário {0} Registrado com sucesso! Faça o login para acessar.", u.Username);
+                    return View("AutenticarUsuario");
+                }
+                else
+                {
+                    throw new System.Exception(Serialized);
+                }
+               
+            }
+            catch (System.Exception ex)
+            {
+                TempData["MensagemErro"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
